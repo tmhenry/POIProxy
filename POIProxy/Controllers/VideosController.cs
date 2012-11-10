c8e148ad60aecc84dd5c5e5a9c4b51b57b33e5dc
Henry Pengye Xia <xiapengye@gmail.com>
2012-11-10 01:26:56 -0600

    Support range request

--- POIProxy/Controllers/VideosController.cs
+++ POIProxy/Controllers/VideosController.cs
@@ -17,14 +17,69 @@ namespace POIProxy.Controllers
 
         public HttpResponseMessage GetVideosByName(string name)
         {
-            string filePath = basePath + name;
-            StreamReader reader = new StreamReader(filePath);
+            Console.WriteLine(Request.Headers.Range);
 
             var response = new HttpResponseMessage();
-            var content = new StreamContent(reader.BaseStream);
-            content.Headers.ContentType = new MediaTypeHeaderValue("video/mp4");
-            response.Content = content;
-            response.Headers.Add("Accept-Ranges", "bytes");
+
+            var requestRange = Request.Headers.Range;
+
+            if (requestRange.Ranges.Count == 1)
+            {
+                RangeItemHeaderValue range = requestRange.Ranges.ElementAt(0);
+
+                if (range.To == null) //Download the whole file
+                {
+                    string filePath = basePath + name;
+                    StreamReader reader = new StreamReader(filePath);
+
+                    var content = new StreamContent(reader.BaseStream);
+                    content.Headers.ContentType = new MediaTypeHeaderValue("video/mp4");
+                    response.Content = content;
+                    response.Headers.Add("Accept-Ranges", "bytes");
+                }
+                else //Send a partial content
+                {
+                    string filePath = basePath + name;
+                    //StreamReader reader = new StreamReader(filePath);
+                    FileStream ins = File.OpenRead(filePath);
+                    
+                    long rangeLength = (long)(range.To - range.From);
+                    long contentLength = ins.Length;
+
+                    if (rangeLength == 0) return response;
+
+                    byte[] buffer = new byte[rangeLength];
+                    ins.Seek((long)range.From, 0);
+                    for (int i = 0; i < rangeLength; i++)
+                    {
+                        buffer[i] = (byte)ins.ReadByte();
+                    }
+                    ins.Close();
+
+                    //reader.BaseStream.Read(buffer, (int)range.From, (int) rangeLength);
+
+                    MemoryStream stream = new MemoryStream(buffer);
+
+                    var content = new StreamContent(stream);
+                    content.Headers.ContentType = new MediaTypeHeaderValue("video/mp4");
+                    response.Content = content;
+                    response.Headers.Add("Accept-Ranges", "bytes");
+                    response.StatusCode = System.Net.HttpStatusCode.PartialContent;
+
+                    
+                    response.Content.Headers.ContentRange = new ContentRangeHeaderValue
+                    (
+                        (long) range.From, 
+                        (long) range.To, 
+                        contentLength
+                    );
+                }
+
+            }
+
+            
+            //response.Content.Headers.ContentRange = new ContentRangeHeaderValue()
+            //response.StatusCode = System.Net.HttpStatusCode.PartialContent;
 
             return response;
         }
